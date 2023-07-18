26ab89971e408bc97f6c22f433a3f063134653d6
guchoo <geronimomembiela@gmail.com>
2023-07-18 19:40:41 -0300

    3.12

--- part3/index.js
+++ part3/index.js
@@ -3,51 +3,21 @@ const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
 
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4
-    }
-]
-
-const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(p => p.id))
-      : 0
-    return maxId + 1
-}
-
+app.use(cors())
 app.use(express.json())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :request-body"))
-app.use(cors())
 
 morgan.token("request-body", (req, res) =>  { 
     return JSON.stringify(req.body)
 })
-
 
 app.get("/", (req, res) => {
     res.send("Welcome to Phonebook API")
 })
 
 app.get("/api/persons", (req, res) => {
-    res.json(persons)
+    Person.find({})
+    .then(person => res.json(person))
 })
 
 app.get("/api/persons/:id", (req, res) => {
