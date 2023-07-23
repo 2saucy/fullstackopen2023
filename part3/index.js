c3d478988560d047f5ad3e168e2ce4807668cb9a
guchoo <geronimomembiela@gmail.com>
2023-07-23 20:28:09 -0300

    3.15, 3.16, 3.17 and 3.18

--- part3/index.js
+++ part3/index.js
@@ -5,20 +5,8 @@ const mongoose = require('mongoose')
 const app = express()
 const Person = require('./models/Person')
 
-
-const persons = [
-    {
-        name: "Joe",
-        number: "00-00-000000"
-    },
-    {
-        name: "Billy",
-        number: "00-00-000000"
-    }
-]
-
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :request-body"))
 
 morgan.token("request-body", (req, res) =>  { 
@@ -29,29 +17,38 @@ app.get("/", (req, res) => {
     res.send("Welcome to Phonebook API")
 })
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (req, res, next) => {
     Person
         .find({})
-        .then(person => res.json(person))
+        .then(result => res.json(result))
+        .catch(err => next(err))
 })
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
     const id = req.params.id
     Person
         .findById(id)
-        .then(person => res.json(person))
-        .catch(err => res.json({ message: err.message }))
-
+        .then(result => {
+            if(result){
+                res.json(result)
+            }
+            else{
+                res.status(404).end()
+            }
+        })
+        .catch(err => next(err))
 })
 
-app.delete("/api/persons/:id", (req, res) => {
-//      const id = Number(req.params.id)
-//      const deletedPerson = persons.filter(person => person.id === id)
-//      persons = persons.filter(person => person.id !== id)
-//      res.json({ message: "the person was successfully removed", deletedPerson: deletedPerson})
+app.delete("/api/persons/:id", (req, res, next) => {
+    const id = req.params.id
+    Person
+        .findByIdAndRemove(id)
+        .then(result => res.status(204).json(result))
+        .catch(err => next(err))
 })
 
-app.post("/api/persons", (req, res) => {
+app.put("/api/persons/:id", (req, res, next) => {
+    const id = req.params.id
     const body = req.body
 
     if (!body.name || !body.number) {
@@ -60,25 +57,61 @@ app.post("/api/persons", (req, res) => {
         })
     }
 
-    const personObject = new Person({
+    const updatedPerson = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person
+        .findByIdAndUpdate(id, updatedPerson, { new: true })
+        .then((result) => res.json(result))
+        .catch(err => next(err))
+})
+
+app.post("/api/persons", (req, res, next) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({ 
+            error: 'content missing' 
+        })
+    }
+
+    const newPerson = new Person({
         name: body.name,
         number: body.number
     })
 
-    personObject
+    newPerson
         .save()
-        .then(() => {
-            console.log(`added ${personObject.name} number ${personObject.number} to phonebook`)
-            mongoose.connection.close()
-        })
+        .then((result) => res.json(result))
+        .catch(err => next(err))
 
-    res.json(personObject)
 })
 
-app.get("/info", (req, res) => {
+app.get("/info", async (req, res) => {
     const currentDate = new Date()
+    const persons = await Person.find({})
     res.send(`Phonebook has info for ${persons.length} people <br> ${currentDate}`)
 })
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: "unknown endpoint" })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
+
+    if(error.name === 'CastError'){
+        return res.status(400).send({ error: "malformated id" })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
