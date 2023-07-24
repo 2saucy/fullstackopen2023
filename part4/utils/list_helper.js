c803a589b25a3be5551e2b89e8807bebdacb80bd
guchoo <geronimomembiela@gmail.com>
2023-07-24 18:29:36 -0300

    4.3, 4.4 and 4.5

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,25 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    let sum = 0
+    blogs.forEach(blog => {
+        sum = sum + blog.likes
+    })
+    return sum
+}
+
+const favoriteBlog = (blogs) => {
+    let maxLikes = -1
+    let fav = null
+    blogs.forEach(blog => {
+        if(blog.likes > maxLikes){
+            maxLikes = blog.likes
+            fav = blog
+        }
+    })
+    return fav
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, }
